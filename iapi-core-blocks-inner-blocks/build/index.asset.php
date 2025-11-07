4bcab4a9f54d1c8ace447d2f7e7d49b8e0104a15
JuanMa <juanma.garrido@gmail.com>
2025-11-07 17:57:44 +0100

    Add IAPI Core Blocks Inner Blocks functionality and dependencies
    - Introduced new block.json file defining the IAPI Core Blocks Inner Blocks with support for inner blocks and interactivity.
    - Added index.js and view.js files for block registration and interactivity handling.
    - Created CSS files for styling the block in both standard and RTL formats.
    - Updated package-lock.json to include new dependencies for interactivity and Preact.
    - Added asset files for managing dependencies and versioning in the build process.

---
+++ iapi-core-blocks-inner-blocks/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks'), 'version' => 'a779f1b079e8fa9342f4');
---
+++ iapi-core-blocks-inner-blocks/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '9accb80f66952af39199', 'type' => 'module');
